afda9c090e2e0c6e56dd381c29b7647b1820ec8c
albertliu6696 <albertliu6696@gmail.com>
2018-08-31 10:49:01 -0700

    Added Functions

--- Antenna.cpp
+++ Antenna.cpp
@@ -1,13 +1,19 @@
 #include "SIMENV.h"
 
+
+Antenna::Antenna(){
+   pos.x = 0;
+   pos.y = 0;
+   mode = 0;
+}
 Antenna::Antenna(Position pos,Signal sig,int type){
    this->pos = pos;
    mode = type; // 0 RX; 1 RX
-   this->sig = sig;
+   this->TX = sig;
 }
 void Antenna::RX(Antenna ant,unsigned int time){
-   if (!type){
+   if (!mode){
       double d = pow( pow(ant.pos.x-pos.x,2)+pow(ant.pos.y-pos.y,2) , 2);
-      voltage = ant.TX.getSignal(d,time);
+      voltage = ant.TX.get_Signal(d,time);
    }
 }
