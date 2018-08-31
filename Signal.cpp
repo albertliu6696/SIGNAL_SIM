8c5d89f86f8e71e8c6110986bb3c76c8424876c7
albertliu6696 <albertliu6696@gmail.com>
2018-08-30 17:23:46 -0700

    Added Preliminary Functions

---
+++ Signal.cpp
@@ -0,0 +1,13 @@
+#include "SIMENV.h"
+
+Signal::Signal(){
+   prop_speed = 300000000;
+};
+double Signal::get_Signal(double distance,unsigned int time){
+   //Default return 1 hertz Signal
+   return cos(distance/prop_speed-time);
+};
+void Signal::set_PropSpeed(double speed){
+   prop_speed = speed;
+};
+
