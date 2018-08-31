7e6852c671e491eb513733331792d1f228f09dc0
albertliu6696 <albertliu6696@gmail.com>
2018-08-30 17:23:24 -0700

    Added Preliminary Functions

---
+++ SIMTime.cpp
@@ -0,0 +1,14 @@
+#include "SIMENV.h"
+
+SIMTime::SIMTime(int t_start){
+   time_sim = t_start;
+};
+unsigned int SIMTime::get_Time(){
+   return time_sim;
+};
+void SIMTime::next_Time(){
+   time_sim = time_sim+1;
+}
+void SIMTime::prev_Time(){
+   time_sim = time_sim==0?time_sim:time_sim-1;
+}
